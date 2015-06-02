fc97d25442b469d695c23b2774022f5987232014
Joshua Newman <jonewman1020@gmail.com>
2015-06-02 16:05:08 -0700

    Basic setup

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp');
+var browserify = require('gulp-browserify');
+var concat = require('gulp-concat');
+
+gulp.task('browserify', function() {
+  gulp.src('src/js/main.js')
+      .pipe(browserify({transform: 'reactify'})) //will turn jsx into JS
+      .pipe(concat('main.js')) //concat into one file
+      .pipe(gulp.dest('dist/js')); //output here
+});
+
+gulp.task('copy', function() {
+  gulp.src('src/index.html')
+      .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['browserify', 'copy']);
+
+gulp.task('watch', function() {
+  gulp.watch('src/**/*.*', ['default']);
+});
+
---
+++ src/js/components/app.js
@@ -0,0 +1,12 @@
+/** @jsx React.DOM */
+var React = require('react');
+
+
+var APP = 
+  React.createClass({
+    render: function() {
+      return <h1>Hello World Im the APP component</h1>
+    }
+  });
+
+module.exports = APP;
